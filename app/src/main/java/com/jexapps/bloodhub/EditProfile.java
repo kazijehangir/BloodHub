f41f1c4ef1f241a3f00450dd147e3df5a6617b8f
khadijatariq <khadijatariqzaman@gmail.com>
2016-11-22 22:56:54 +0500

    user profile updated

--- app/src/main/java/com/jexapps/bloodhub/EditProfile.java
+++ app/src/main/java/com/jexapps/bloodhub/EditProfile.java
@@ -9,5 +9,6 @@ public class EditProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        //TODO: Ask for contact number and age
     }
 }
