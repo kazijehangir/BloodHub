d9269676ee9bb84fba077cdeab5e1ef31199cea3
khadijatariq <khadijatariqzaman@gmail.com>
2016-11-23 20:35:01 +0500

    emergency request update

--- app/src/main/java/com/jexapps/bloodhub/EmergencyRequestActivity.java
+++ app/src/main/java/com/jexapps/bloodhub/EmergencyRequestActivity.java
@@ -2,7 +2,14 @@ package com.jexapps.bloodhub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 
 public class EmergencyRequestActivity extends AppCompatActivity {
@@ -15,7 +22,6 @@ public class EmergencyRequestActivity extends AppCompatActivity {
 
         LinearLayout rlayout = (LinearLayout) findViewById(R.id.activity_emergency_request);
         setTitle("EMERGENCY REQUEST");
-
 
     }
 }
