337acb230e60a4abdc6ddd3ea1729d74b7b770fd
aimengaba <aimengaba@gmail.com>
2016-11-24 09:02:56 +0500

    faqs

--- app/src/main/java/com/jexapps/bloodhub/FaqFragment.java
+++ app/src/main/java/com/jexapps/bloodhub/FaqFragment.java
@@ -7,8 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -57,7 +61,8 @@ public class FaqFragment extends Fragment {
     ExpandableListView expandableListView;
 
     List<String> faq;
-    Map<String, List<String>> faqDet;
+    Map<String, List<String>> topics;
+    ExpandableListAdapter listAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -67,7 +72,6 @@ public class FaqFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
 
-        //expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
 
     }
 
@@ -75,7 +79,58 @@ public class FaqFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_faq, container, false);
+        View rootView = (View) inflater.inflate(R.layout.fragment_faq, container, false);
+        expandableListView = (ExpandableListView) rootView.findViewById(R.id.expandableListView);
+        fillData();
+
+
+        listAdapter = new myExpListAdapter(this,faq,topics);
+
+        expandableListView.setAdapter(listAdapter);
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView expandableListView, View view, int i, int i1, long l) {
+
+                Toast.makeText(MainActivity, this, faq.get(i) + " : " + topics.get(faq.get(i)).get(i1), Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
+        return rootView;
+    }
+
+    public void fillData()
+    {
+        faq = new ArrayList<>();
+        topics = new HashMap<>();
+
+        faq.add("Eligible to donate Blood"); //faq0
+        faq.add("Is donating blood safe?"); //faq1
+        faq.add("How long will it take for the body to replenish the blood?"); //faq2
+        faq.add("Time period after which you can donate blood again"); //faq3
+        faq.add("Can a donor work after donating blood?"); //faq4
+
+        List<String> faq0 = new ArrayList<>();
+        List<String> faq1 = new ArrayList<>();
+        List<String> faq2 = new ArrayList<>();
+        List<String> faq3 = new ArrayList<>();
+        List<String> faq4 = new ArrayList<>();
+
+        faq0.add("Any person within the age group of 18-60 years with a body weight as minimum 45kgs, having haemoglobin content as minimum 12.5 gm%");
+        faq1.add("Donating blood is a safe process. Needles and bags used to collect blood are used only once and then discarded");
+        faq1.add("Its highly reccommended");
+        faq2.add("The body replaces blood volume or plasma within 24 hours. Red cells need about 21 days for complete replacement.");
+        faq3.add("A healthy person can donate after every 56 days.");
+        faq3.add("So basically after every 3 months");
+        faq4.add("Of course! Routine work is absolutely fine after the initial rest. Rigorous physical work should be avoided for a few hours.");
+
+        topics.put(faq.get(0), faq0);
+        topics.put(faq.get(1), faq1);
+        topics.put(faq.get(2), faq2);
+        topics.put(faq.get(3), faq3);
+        topics.put(faq.get(4), faq4);
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
---
+++ app/src/main/java/com/jexapps/bloodhub/myExpListAdapter.java
@@ -0,0 +1,105 @@
+package com.jexapps.bloodhub;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by aimengaba on 24/11/2016.
+ */
+public class myExpListAdapter extends BaseExpandableListAdapter
+{
+
+
+    Context context;
+    List<String> faq;
+    Map<String, List<String>> topics;
+
+    public myExpListAdapter(FaqFragment context, List<String> topics, Map<String, List<String>> faq) {
+        this.context = context;
+        this.topics = topics;
+        this.faq = faq;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return faq.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return topics.get(faq.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return faq.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return topics.get(faq.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup)
+    {
+
+        String faq = (String) getGroup(i);
+
+        if(view == null)
+        {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.list_parent, null);
+        }
+
+        TextView txtParent = (TextView) view.findViewById(R.id.txtParent);
+        txtParent.setText(faq);
+
+        return view;
+    }
+
+    @Override
+    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup)
+    {
+
+        String topic = (String) getChild(i, i1);
+
+        if(view == null)
+        {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.list_child, null);
+        }
+
+        TextView txtChild = (TextView) view.findViewById(R.id.txtChild);
+        txtChild.setText(topic);
+
+        return view;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return true;
+    }
+}
