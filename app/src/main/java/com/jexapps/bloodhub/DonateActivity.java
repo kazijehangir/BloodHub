4d3f2effd881df3067eb7ab6b43850ad7d68bdf9
khadijatariq <khadijatariqzaman@gmail.com>
2016-11-28 11:18:50 +0500

    add patient

--- app/src/main/java/com/jexapps/bloodhub/DonateActivity.java
+++ app/src/main/java/com/jexapps/bloodhub/DonateActivity.java
@@ -1,13 +1,40 @@
 package com.jexapps.bloodhub;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DonateActivity extends AppCompatActivity {
-
+    Dialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_donate);
+        setContentView(R.layout.popup_donate);
+        setTitle("DONATE BLOOD");
+        Button submit = (Button) findViewById(R.id.submit_button);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog = new Dialog(DonateActivity.this);
+                dialog.setTitle("Donate Blood");
+                dialog.setContentView(R.layout.popup_submit);
+                dialog.show();
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                final Button request = (Button) dialog.findViewById(R.id.button_ok);
+                request.setOnClickListener(new View.OnClickListener(){
+
+                    @Override
+                    public void onClick(View view) {
+                        dialog.cancel();
+                    }
+                });
+            }
+        });
     }
 }
