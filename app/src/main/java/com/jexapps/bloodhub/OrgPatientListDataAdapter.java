1f228838c5d159c1179c9e32594a2ffbf868e33f
Jehangir Kazi <kazi.jehangir@gmail.com>
2016-12-08 06:35:05 +0500

    Added lists through recyclerview for organizations home

---
+++ app/src/main/java/com/jexapps/bloodhub/OrgPatientListDataAdapter.java
@@ -0,0 +1,115 @@
+package com.jexapps.bloodhub;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Jehangir Kazi on 23/11/16.
+ * This file is supposed to be used as an adapter with the RequestListFragment.java
+ * and the request_card_view.xml file.
+ */
+
+public class OrgPatientListDataAdapter extends RecyclerView.Adapter<OrgPatientListDataAdapter.ViewHolder> {
+    private String[] mDataset;
+    private final Context mContext;
+    private static String mEmail;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        // each data item is just a string in this case
+        public TextView mName, mLocation, mNeeds, mWhen, mDiagnosis, mTransport;
+        public ImageView mImage, mTransportImage;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mName = (TextView) itemView.findViewById(R.id.name_text);
+            mLocation = (TextView) itemView.findViewById(R.id.location_text);
+            mNeeds = (TextView) itemView.findViewById(R.id.needs_text);
+            mWhen = (TextView) itemView.findViewById(R.id.when_text);
+            mDiagnosis = (TextView) itemView.findViewById(R.id.diagnosis_text);
+            mImage = (ImageView) itemView.findViewById(R.id.request_picture);
+            mTransportImage = (ImageView) itemView.findViewById(R.id.transport_image);
+            mTransport = (TextView) itemView.findViewById(R.id.transport_text);
+            itemView.findViewById(R.id.card_view).setOnClickListener(this);
+        }
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(v.getContext(), OrgPatientDetail.class);
+            TextView mName = (TextView) itemView.findViewById(R.id.name_text);
+            intent.putExtra("name", mName.getText());
+            TextView mNeeds = (TextView) itemView.findViewById(R.id.needs_text);
+            intent.putExtra("needs", mNeeds.getText());
+            TextView mLocation = (TextView) itemView.findViewById(R.id.location_text);
+            intent.putExtra("location", mLocation.getText());
+            TextView mWhen = (TextView) itemView.findViewById(R.id.when_text);
+            intent.putExtra("when", mWhen.getText());
+            TextView mDiagnosis = (TextView) itemView.findViewById(R.id.diagnosis_text);
+            intent.putExtra("diagnosis", mDiagnosis.getText());
+            TextView mTransport = (TextView) itemView.findViewById(R.id.transport_text);
+            intent.putExtra("transport", mTransport.getText());
+            intent.putExtra("mEmail", mEmail);
+            v.getContext().startActivity(intent);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public OrgPatientListDataAdapter(String[] myDataset, Context context, String email) {
+        mDataset = myDataset;
+        mContext = context;
+        mEmail = email;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public OrgPatientListDataAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                                 int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.org_patient_card_view, parent, false);
+        return new ViewHolder(v);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        String[] strings = mDataset[position].split(":");
+        holder.mName.setText(strings[0]);
+        holder.mNeeds.setText(strings[1]);
+        holder.mLocation.setText(strings[2]);
+        holder.mWhen.setText(strings[3]);
+        if (strings[3].equals("URGENT")) {
+            holder.mWhen.setTextColor(0xFFFF0000);
+        }
+        holder.mDiagnosis.setText(strings[4]);
+        if (strings[5].equals("Male")) {
+            holder.mImage.setImageDrawable(ContextCompat.getDrawable(mContext,R.drawable.boy));
+        } else if (strings[5].equals("Female")) {
+            holder.mImage.setImageDrawable(ContextCompat.getDrawable(mContext, R.drawable.girl));
+        }
+        if (strings[6].equals("Yes")) {
+            holder.mTransport.setText("Available");
+            holder.mTransportImage.setImageDrawable(ContextCompat.getDrawable(mContext,R.drawable.ic_car));
+        } else if (strings[6].equals("No")) {
+            holder.mTransport.setText("Not Available");
+            holder.mTransportImage.setImageDrawable(ContextCompat.getDrawable(mContext,R.drawable.ic_no_car));
+        }
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
+
--- app/src/main/java/com/jexapps/bloodhub/OrgPatientListFragment.java
+++ app/src/main/java/com/jexapps/bloodhub/OrgPatientListFragment.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,21 +15,26 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link OrgPatientListFragment.OnFragmentInteractionListener} interface
+ * {@link OrgDonorListFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link OrgPatientListFragment#newInstance} factory method to
+ * Use the {@link OrgDonorListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class OrgPatientListFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String[] dummyDataset = new String[] {
+            "Jamshed:2 bags of O-:National Hospital:URGENT:Surgery:Male:Yes",
+            "Aliya:1 bag of A+:Red Crescent:Today:Thalassemia:Female:No",
+            "Hamid:1 bag of AB-:Shaukat Khanum:Next Week:Cancer:Male:No",
+            "Saniya:1 bags of B+:Aadil Hospital:Today:Accident:Female:Yes"
+    };
+    private String mEmail;
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     private OnFragmentInteractionListener mListener;
 
     public OrgPatientListFragment() {
@@ -37,16 +45,14 @@ public class OrgPatientListFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OrgPatientListFragment.
+     * @param email Parameter 1.
+     * @return A new instance of fragment OrgDonorListFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static OrgPatientListFragment newInstance(String param1, String param2) {
+    public static OrgPatientListFragment newInstance(String email) {
         OrgPatientListFragment fragment = new OrgPatientListFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString("mEmail", email);
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,16 +61,34 @@ public class OrgPatientListFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mEmail = getArguments().getString("mEmail");
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_org_patient_list, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_org_patient_list, container, false);
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.org_patient_list_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.addItemDecoration(new RecycleMarginDecoration(getActivity()));
+
+
+        // specify an adapter (see also next example)
+        mAdapter = new OrgPatientListDataAdapter(dummyDataset, getContext(), mEmail);
+        mRecyclerView.setAdapter(mAdapter);
+
+        return rootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
