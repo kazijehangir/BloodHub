54eb7a536c68236ff5b54598e9dc5399d093e93e
khadijatariq <khadijatariqzaman@gmail.com>
2017-07-24 00:10:50 +0500

    Map API Key

--- app/src/main/java/com/jexapps/bloodhub/RequestMapFragment.java
+++ app/src/main/java/com/jexapps/bloodhub/RequestMapFragment.java
@@ -20,61 +20,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.OnMapReadyCallback;
 
-
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link RequestMapFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link RequestMapFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class RequestMapFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    MapView mMapView;
+    private GoogleMap googleMap;
     private static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1;
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     private OnFragmentInteractionListener mListener;
 
     public RequestMapFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RequestMapFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RequestMapFragment newInstance(String param1, String param2) {
-        RequestMapFragment fragment = new RequestMapFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    MapView mMapView;
-    private GoogleMap googleMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
