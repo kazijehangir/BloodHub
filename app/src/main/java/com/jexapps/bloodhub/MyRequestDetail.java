9ff9906e0ae0b5d438457d5689ded853f17455c2
mahnoormmalik <mahnoormmalik@gmail.com>
2018-03-23 01:56:50 +0500

    layoutss

--- app/src/main/java/com/jexapps/bloodhub/MyRequestDetail.java
+++ app/src/main/java/com/jexapps/bloodhub/MyRequestDetail.java
@@ -6,7 +6,15 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.jexapps.bloodhub.m_Model.BloodRequest;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 /**
  * Created by mahnoor on 19/10/2017.
@@ -16,24 +24,39 @@ public class MyRequestDetail extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseUser user;
     DatabaseReference db;
-    String name, when, location;
+    String name, when, location, request;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_my_request_details);
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
-        if(savedInstanceState == null){
-
-        } else {
-            name = (String) savedInstanceState.getSerializable("name");
-            when = (String) savedInstanceState.getSerializable("when");
-            location = (String) savedInstanceState.getSerializable("location");
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                request = null;
+            } else {
+                request = extras.getString("request");
+            }
         }
-        TextView mName = (TextView) findViewById(R.id.name);
-        mName.setText(name);
-        TextView mTime = (TextView) findViewById(R.id.time);
-        mTime.setText(when);
-        TextView mLocation = (TextView) findViewById(R.id.location);
-        mLocation.setText(location);
+        if(request!=null && !request.isEmpty()){
+            FirebaseDatabase.getInstance().getReference().child("bloodrequests").child(request).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    BloodRequest data = dataSnapshot.getValue(BloodRequest.class);
+
+                    TextView mName = (TextView) findViewById(R.id.name);
+                    mName.setText(name);
+                    TextView mTime = (TextView) findViewById(R.id.time);
+                    mTime.setText(when);
+                    TextView mLocation = (TextView) findViewById(R.id.location);
+                    mLocation.setText(location);
+
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            });
+        }
+
     }
 }
